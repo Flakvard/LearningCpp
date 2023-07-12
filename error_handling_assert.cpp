1d4d484cad9086e8119717292c1bd7de5a79e14f
flakvard <marnifalkvard@gmail.com>
2023-07-12 15:52:45 +0100

    update with char

--- error_handling_assert.cpp
+++ error_handling_assert.cpp
@@ -1,16 +1,25 @@
 #include <cassert>
 #include <iostream>
 #include <string>
+#include <cstring>
 
 void print(std::string name);
+void printChar(char name[]);
 
 int main(){
-    std::string bob = "Tom";
+    std::string bob = "Bob";
     print(bob);
+
+    char bobby[] = "Bob";
+    printChar(bobby);
 
 }
 
 void print(std::string name){
     assert(name == "Bob");
+    std::cout<<"The name is "<<name<<'\n';
+}
+void printChar(char name[]){
+    assert(strcmp(name, "Bob") == 0);
     std::cout<<"The name is "<<name<<'\n';
 }
