7cae8d0eef9449f17b54d8794ea9a0e0df4b172c
flakvard <marnifalkvard@gmail.com>
2023-09-18 03:58:16 +0200

    refactor, create student

--- linked_list.cpp
+++ linked_list.cpp
@@ -8,18 +8,25 @@ struct studentLinkedList{
 };
 typedef studentLinkedList * studentCollection;
 
+studentLinkedList* createStudent(std::string name, int grade);
+
 int main() {
 
     studentCollection sc;
-    studentLinkedList * james = new studentLinkedList;
-    james->studentName = "James";
-    james->studentGrade = 12;
+    studentLinkedList * james = createStudent("James", 12);
     sc->next = james;
+    james = NULL;
     std::cout<<sc->next->studentName<<'\n';
-    
 
 }
 
+studentLinkedList* createStudent(std::string name, int grade){
+    studentLinkedList * student = new studentLinkedList;
+    student->studentName = name;
+    student->studentGrade = grade;
+    return student;
+
+}
 void linkTester(){
 
 }
