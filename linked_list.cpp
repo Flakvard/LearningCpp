d9e5f22dc94f8f9f4ba4bd102965111196ae2715
flakvard <marnifalkvard@gmail.com>
2023-09-18 13:55:58 +0200

    print linked list

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,32 +1,67 @@
 #include <string>
 #include <iostream>
 
-struct studentLinkedList{
+struct studentNode{
     std::string studentName;
     int studentGrade;
-    studentLinkedList * next;
+    studentNode * next;
 };
-typedef studentLinkedList * studentCollection;
+typedef studentNode * studentCollection;
 
-studentLinkedList* createStudent(std::string name, int grade);
+studentNode* createStudent(std::string name, int grade);
+void linkStudent(studentCollection sc, studentNode *student);
+void printStudentCollection(studentCollection sc);
 
 int main() {
 
     studentCollection sc;
-    studentLinkedList * james = createStudent("James", 12);
+    //sc->next = 0;
+    studentNode * james = createStudent("James", 12);
+    studentNode * fred = createStudent("Fred", 10);
+    studentNode * jeff = createStudent("Jeff", 02);
     sc->next = james;
-    james = NULL;
-    std::cout<<sc->next->studentName<<'\n';
+    james->next = fred;
+    fred->next = jeff;
+    // linkStudent(sc, james);
+    // linkStudent(sc, fred);
+    // linkStudent(sc, jeff);
+    printStudentCollection(sc);
+
+}
+void printStudentCollection(studentCollection sc){
+    studentNode * loopPtr = sc;
+    if(loopPtr->next == NULL) return;
+
+    while (loopPtr->next != NULL)
+    {
+        std::cout<<loopPtr->next->studentName<<'\n';
+        std::cout<<loopPtr->next->studentGrade<<'\n';
+        std::cout<<'\n';
+        loopPtr = loopPtr->next;
+    }
 
 }
 
-studentLinkedList* createStudent(std::string name, int grade){
-    studentLinkedList * student = new studentLinkedList;
+studentNode* createStudent(std::string name, int grade){
+    studentNode * student = new studentNode;
     student->studentName = name;
     student->studentGrade = grade;
+    student->next = NULL;
     return student;
 
 }
-void linkTester(){
+
+void linkStudent(studentCollection sc, studentNode *student){
+    student->next = sc;
+    //studentNode * lastStudent;
+    // while (sc->next != NULL)
+    // {
+    //     lastStudent = sc;
+    // }
+    // lastStudent->next = student;
+    // student->next = sc;
+    // sc->next = student;
+    // student = NULL;
+
 
 }
