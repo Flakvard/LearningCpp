73ffa33fe8f473eac6c0b85a8d4e8891a100727a
flakvard <marnifalkvard@gmail.com>
2023-09-21 12:55:56 +0200

    student blueprint

--- dynamic_class.cpp
+++ dynamic_class.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
 
+struct studentRecord{
+    int id = 0;
+    int grade = 0;
+};
+
+class studentCollection
+{
+private:
+    struct studentNode
+    {
+        studentRecord studentData;
+        studentNode* next;
+    };
+public:
+    studentCollection(/* args */);
+    addRecord();
+    studentRecord recordWithNumber(int idNum);
+    void removeRecord();
+    ~studentCollection();
+private:
+    typedef studentNode * studentList;
+    studentList _listHead;
+};
+
+studentCollection::studentCollection(/* args */)
+{
+    studentCollection::_listHead = NULL;
+}
+
+studentCollection::addRecord()
+{
+}
+
+studentRecord studentCollection::recordWithNumber(int idNum)
+{
+    //return studentRecord();
+}
+
+studentCollection::~studentCollection()
+{
+}
+
+
 int main(){
 
-    std::cout<<"hello world";
-    
 
 
 }
