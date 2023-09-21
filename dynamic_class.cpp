68abd562cfd25fb7838780067d7b4fbb63e1f1b1
flakvard <marnifalkvard@gmail.com>
2023-09-21 14:45:23 +0200

    update, working print and add to record

--- dynamic_class.cpp
+++ dynamic_class.cpp
@@ -2,9 +2,26 @@
 #include <string>
 
 struct studentRecord{
-    int id = 0;
-    int grade = 0;
-    std::string name = "";
+    int id;
+    int grade;
+    std::string name;
+
+    studentRecord(){
+        int id = 0;
+        int grade = 0;
+        std::string name = "";
+    }
+    studentRecord(int id, int grade,std::string name) :id(id), grade(grade),name(name){}
+    void printStudent(){
+    std::cout<<"ID: "
+                <<id
+                <<"\nName: "
+                <<name
+                <<"\nGrade: "
+                <<grade
+                <<'\n';
+    }
+
 };
 
 class studentCollection
@@ -20,6 +37,7 @@ public:
     void addRecord(studentRecord studentData);
     studentRecord recordWithNumber(int idNum);
     void removeRecord();
+    void printRecords();
     ~studentCollection();
 private:
     typedef studentNode * studentList;
@@ -57,6 +75,32 @@ void studentCollection::removeRecord()
 {
 }
 
+void studentCollection::printRecords(){
+    studentNode * loopPtr = _listHead;
+    while (loopPtr->next != NULL){
+        std::cout<<"ID: "
+                 <<loopPtr->studentData.id
+                 <<"\nName: "
+                 <<loopPtr->studentData.name
+                 <<"\nGrade: "
+                 <<loopPtr->studentData.grade
+                 <<'\n';
+        loopPtr = loopPtr->next;
+    }
+    if (loopPtr == NULL){
+        return;
+    }else{
+        std::cout<<"ID: "
+                 <<loopPtr->studentData.id
+                 <<"\nName: "
+                 <<loopPtr->studentData.name
+                 <<"\nGrade: "
+                 <<loopPtr->studentData.grade
+                 <<'\n';
+    }
+    
+}
+
 studentCollection::~studentCollection()
 {
 }
@@ -65,12 +109,28 @@ studentCollection::~studentCollection()
 int main(){
 
     studentCollection sc;
-    studentRecord charles;
-    charles.id = 1;
-    charles.grade = 12;
-    charles.name = "Charles";
-    sc.addRecord(charles);
+    {
+        studentRecord charles;
+        charles.id = 1;
+        charles.grade = 12;
+        charles.name = "Charles";
+        sc.addRecord(charles);
+    }
+    sc.printRecords();
+    {
+        studentRecord bobby(2,4,"Bobby");
+        sc.addRecord(bobby);
+    }
+    {
+        studentRecord tommy(3,7,"Tommy");
+        sc.addRecord(tommy);
+    }
+    sc.printRecords();
 
+    studentRecord idTwoStudent = sc.recordWithNumber(2);
+    studentRecord idOneStudent = sc.recordWithNumber(1);
+    idOneStudent.printStudent();
+    idTwoStudent.printStudent();
 
 
 }
