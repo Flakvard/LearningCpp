033401b3933fcab09dce58be583439d824e2ab7e
flakvard <marnifalkvard@gmail.com>
2023-09-22 09:42:46 +0200

    refactor, dispatch to a recursiveSumArr naming

--- recursion_functions.cpp
+++ recursion_functions.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
 
-
-int sumOfArr(int setofnumbers[], int size){
-    int sum = 0;
-    for (size_t i = 0; i < 4; i++){
-        sum += setofnumbers[i];
-    }
-    return sum;
-}
-
-int dispacterSum(int setofnumbers[], int size){
+int recursiveSumArr(int setofnumbers[], int size){
     if (size == 0) return 0;
     int sum = setofnumbers[size-1];
-    sum += dispacterSum(setofnumbers, size-1);
+    sum += recursiveSumArr(setofnumbers, size-1);
     return sum;
 }
 
@@ -21,7 +12,7 @@ int main(){
     int setofnumbers[] = {1,3,6,2}; // 12
     int sum = 0;
     int count = 4;
-    sum = dispacterSum(setofnumbers, count);
+    sum = recursiveSumArr(setofnumbers, count);
 
     std::cout<<sum<<'\n';
 }
