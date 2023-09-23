c9ab9baee6a8373c03e4729a7d87d8d93815c084
flakvard <marnifalkvard@gmail.com>
2023-09-23 13:45:49 +0200

    recursive count all tree leafs

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -17,12 +17,14 @@ private:
     };
     void printLeaf(binaryNode* node);
     double sumLeaf(binaryNode *node);
+    int countLeaf(binaryNode *node);
     
 public:
     BinaryTree();
     void appendTree(treeData node);
     void printTree();
     double sumTree();
+    int countTree();
     ~BinaryTree(){};
 private:
     typedef binaryNode* treeNodes;
@@ -113,27 +115,55 @@ double BinaryTree::sumLeaf(binaryNode *node){
     return sum;
 
 }
-double BinaryTree::sumTree(){
+int BinaryTree::countLeaf(binaryNode *node)
+{
+    int count = 0;
+    if (node == NULL)
+        return count;
+
+    ++count;
+
+    // Reach bottom
+    if (node->nextLeft == NULL && node->nextRight == NULL){
+        return count;
+    }
+
+    count += countLeaf(node->nextLeft);
+    count += countLeaf(node->nextRight);
+    
+    return count;
+    
+}
+double BinaryTree::sumTree()
+{
     binaryNode * tree = _root;
     return sumLeaf(tree);
 }
 
+int BinaryTree::countTree()
+{
+    binaryNode * tree = _root;
+    return countLeaf(tree);
+}
 
 int main(){
 
     BinaryTree bt;
-    treeData jippy;    jippy.id = 1;    jippy.amount  = 150.20;   jippy.name  = "Jippy";    bt.appendTree(jippy); // HEAD  y
-    treeData tommy;    tommy.id = 2;    tommy.amount  = 252.60;   tommy.name  = "Tommy";    bt.appendTree(tommy); // Left  y
-    treeData booby;    booby.id = 3;    booby.amount  = -55.22;   booby.name  = "Booby";    bt.appendTree(booby); // Right b
-    treeData wompy;    wompy.id = 4;    wompy.amount  = 159.98;   wompy.name  = "Wompy";    bt.appendTree(wompy); // Left  y
-    treeData marko;    marko.id = 5;    marko.amount  =  73.32;   marko.name  = "Marko";    bt.appendTree(marko); // Right y
-    treeData jumper;   jumper.id = 6;   jumper.amount = -42.47;   jumper.name = "Jumper";   bt.appendTree(jumper);// Left  y
-    treeData dillan;   dillan.id = 7;   dillan.amount = 112.63;   dillan.name = "Dillan";   bt.appendTree(dillan);// Right y
-    treeData noob;     noob.id = 9;     noob.amount   =  22.22;   noob.name   = "Noob" ;    bt.appendTree(noob);  // Left  y
-    treeData slupp;    slupp.id = 10;   slupp.amount  = -12.78;   slupp.name  = "Slupp";    bt.appendTree(slupp); // Right y
-    treeData jolp;     jolp.id = 11;    jolp.amount   =  73.32;   jolp.name   = "Jolp" ;    bt.appendTree(jolp);  // Left  y
-    treeData prump;    prump.id = 12;   prump.amount  = -42.47;   prump.name  = "Prump";    bt.appendTree(prump); // Right y
+    treeData jippy;    jippy.id = 1;    jippy.amount  = 150.20;   jippy.name  = "Jippy";    bt.appendTree(jippy); // HEAD 
+    treeData tommy;    tommy.id = 2;    tommy.amount  = 252.60;   tommy.name  = "Tommy";    bt.appendTree(tommy); // Left 
+    treeData booby;    booby.id = 3;    booby.amount  = -55.22;   booby.name  = "Booby";    bt.appendTree(booby); // Right
+    treeData wompy;    wompy.id = 4;    wompy.amount  = 159.98;   wompy.name  = "Wompy";    bt.appendTree(wompy); // Left 
+    treeData marko;    marko.id = 5;    marko.amount  =  73.32;   marko.name  = "Marko";    bt.appendTree(marko); // Right
+    treeData jumper;   jumper.id = 6;   jumper.amount = -42.47;   jumper.name = "Jumper";   bt.appendTree(jumper);// Left 
+    treeData dillan;   dillan.id = 7;   dillan.amount = 112.63;   dillan.name = "Dillan";   bt.appendTree(dillan);// Right
+    treeData noob;     noob.id = 9;     noob.amount   =  22.22;   noob.name   = "Noob" ;    bt.appendTree(noob);  // Left 
+    treeData slupp;    slupp.id = 10;   slupp.amount  = -12.78;   slupp.name  = "Slupp";    bt.appendTree(slupp); // Right
+    treeData jolp;     jolp.id = 11;    jolp.amount   =  73.32;   jolp.name   = "Jolp" ;    bt.appendTree(jolp);  // Left 
+    treeData prump;    prump.id = 12;   prump.amount  = -42.47;   prump.name  = "Prump";    bt.appendTree(prump); // Right
     bt.printTree();
     double sum = bt.sumTree();
     std::cout<<"Sum of total tree = "<<sum<<'\n';
+
+    int size = bt.countTree();
+    std::cout<<"Total size of tree = "<<size<<'\n';
 }
