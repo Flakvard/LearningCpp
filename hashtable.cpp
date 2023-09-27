9a08cb0f9c3ac6644b4b6b3369fa5baa646d7406
flakvard <marnifalkvard@gmail.com>
2023-09-27 09:51:33 +0200

    update

--- hashtable.cpp
+++ hashtable.cpp
@@ -1,31 +1,55 @@
 #include <string>
 #include <iostream>
+#include <random>
 
 class hashtable
 {
-private:
-    int bucketSize;
-    char hashTable[0];
-
 public:
     hashtable();
     ~hashtable();
     int hashFunction(std::string index);
+    void testTable();
+    //void printTable();
+    int getBucketSize();
+    char* gethashtable();
+
+private:
+    int bucketSize = 10;
+    char hashTable[10];
 };
+
+void hashtable::testTable(){
+    int size = getBucketSize();
+    char* arr = gethashtable();
+    for (int i = 0; i < size; i++){
+        arr[i] = std::rand() % 100;
+    }
+
+}
+
+int  hashtable::getBucketSize(){
+    return bucketSize;
+}
+char*  hashtable::gethashtable(){
+    return hashTable;
+}
 
 hashtable::hashtable()
 {
+    // Nothing to do yet
 }
 
 hashtable::~hashtable()
 {
+    // Nothing to do yet
 }
 int hashFunction(std::string index){
     return 0;
 }
 
 int main(){
-    std::cout<<"Hello world\n";
     char* arr[10];
     hashtable ht;
+    ht.testTable();
+
 }
